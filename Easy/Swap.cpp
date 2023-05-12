4a9b0e18b5a4c9ba4f41c297162d2cda2f2fac14
Dhruv Malhotra <dhruvmalhotra2911@gmail.com>
2023-05-12 22:58:55 +0530

    Solution for swapping every ith element of the array with (i+2)th element

---
+++ Easy/Swap.cpp
@@ -0,0 +1,12 @@
+  void swapElements(int arr[], int sizeof_array){
+
+        // Your code here
+
+        for(int i=0; i<sizeof_array-2; i++)
+        {
+            swap(arr[i],arr[i+2]);
+        }
+
+        return;
+
+    }
