22fc17922839e796ba6c4808512ee1c8d61c2b4b
Dhruv Malhotra <dhruvmalhotra2911@gmail.com>
2023-05-14 22:59:08 +0530

    Solution for printing the even number first and odd number next in individual lines

---
+++ Easy/EvenOdd.cpp
@@ -0,0 +1,13 @@
+ void evenOdd(int a, int b){
+        // Code here
+
+        if(a%2==0 and b%2!=0)
+        {
+            cout<<a<<endl<<b<<endl;
+        }
+
+        else
+        {
+            cout<<b<<endl<<a<<endl;
+        }
+    }
